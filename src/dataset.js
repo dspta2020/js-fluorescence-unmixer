ffde4bbb2948e1e4690a3bff423108c3eee7d45d
dspta2020 <BigDotLilDot@yahoo.com>
2024-12-21 18:58:24 -0800

    created first cut dataset class

--- src/dataset.js
+++ src/dataset.js
@@ -1,17 +1,25 @@
+const path = require('path');
+
 // src/dataset.js
 const fs = require('fs').promises
 
 class Dataset{
 
     constructor(path_to_data){
+        // path to the file
         this.filepath = path_to_data
+
+        // data to read
         this.wavels = [];
         this.data = [];
         this.headers = [];
+
+        // derived from read data
         this.wavel_max = 0;
         this.wavel_min = 0;
     }
 
+    // function for loading in the raw data from a csv file
     async loadFromCsv(){
         try{
             // log progress 
@@ -28,17 +36,44 @@ class Dataset{
             for (let nth_line = 0; nth_line < num_lines; nth_line++){
                 
                 // get the nth line 
-                let line = lines[nth_line].split(',')
+                let line = lines[nth_line].split(',');
 
-                // print it out atm
+                // grab the data
                 if (nth_line > 0){
-                    this.wavels.push(line[0])
+                    // add the wavelength value to the wavel vector
+                    this.wavels.push(+line[0]);
+                    // add the data vector for each of the dyes to the nth row of the data array
+                    // essentially represeting a 2D array as a list of lists
+                    this.data.push(line.slice(1).map(Number));
+                }
+                // grab the dye names
+                else if (nth_line == 0){
+                    // this will add the array of sample names to the headers attribute
+                    this.headers = line.slice(1);
                 }
             }
         }
+        // if the file cant be read or throws an error during grabbing the data
         catch (err){
             console.error('Error reading file :', err);
         }
+        // once all the data is read in then just grab the max and min values
+        this.getWavelMinMax(this.wavels);
+    }
+
+    getWavelMinMax(){
+        // we'll need these for interpolation later
+        this.wavel_max = Math.max(...this.wavels);
+        this.wavel_min = Math.min(...this.wavels);
+    }
+
+    getDyeVector(dye_index){
+        // return an object that tells you which dye vector is returned and then also the data
+        let dye_data = this.data.map(row => row[dye_index]);
+        let dye_name = this.headers[dye_index];
+
+        // returns an object as dye_name and dye_data
+        return {dye_name, dye_data}
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -2,13 +2,26 @@ const Dataset = require('./dataset');
 
 (async () => {
 
-    path_to_data = './data/components.csv'
-    const dataset = new Dataset(path_to_data);
+    path_to_components = './data/components.csv'
+    path_to_test_data = './data/test_data.csv'
+
+    const component_data = new Dataset(path_to_components);
+    const test_data = new Dataset(path_to_test_data);
 
     // load the csv data
-    await dataset.loadFromCsv()
+    await component_data.loadFromCsv()
+    await test_data.loadFromCsv()
 
-    console.log(dataset.wavels)
+    // check some attributes of the data
+    // console.log(dataset.wavel_max, dataset.wavel_min, dataset.headers, dataset.data.map(row => row[1])
+    // const columnValues = dataset.data.map(row => row[1]);
+    // console.log(dataset.headers[1])
+
+    // let dye_vector = test_data.getDyeVector(1)
+    // console.log(dye_vector.dye_name, dye_vector.dye_data)
+
+    // dye_vector = test_data.getDyeVector(153)
+    // console.log(dye_vector.dye_name, dye_vector.dye_data)
 
     console.log('Finished loading data.')
 })();
